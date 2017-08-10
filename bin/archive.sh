7d2410e91597f894039c4d77f011db8c57bf80fb
Toshimitsu Takahashi <toshi@tilfin.com>
2017-08-10 10:43:50 +0900

    Support firefox and chrome, use sanitizer for html injection

--- bin/archive.sh
+++ bin/archive.sh
@@ -4,7 +4,17 @@
 #--
 zipfile=aws-extend-switch-roles.zip
 
+cd dist/chrome;
+\rm $zipfile
+zip -r $zipfile \
+  manifest.json *.html icons/ js/ 
+echo "archived: chrome/$zipfile"
+
+echo "----"
+
+cd ../firefox;
+\rm $zipfile
 zip -r $zipfile \
   manifest.json *.html icons/ js/ 
 
-echo "archived: $zipfile"
+echo "archived: firefox/$zipfile"
--- bin/build.sh
+++ bin/build.sh
@@ -2,8 +2,12 @@
 #--
 # build.sh
 #--
-content=js/content.js
-popup=js/popup.js
+
+mkdir -p dist/chrome/js
+mkdir -p dist/firefox/js
+
+content=dist/chrome/js/content.js
+popup=dist/chrome/js/popup.js
 
 cat src/lib/profile.js         > $content
 cat src/content.js            >> $content
@@ -12,4 +16,17 @@ cat src/lib/load_aws_config.js > $popup
 cat src/lib/color_picker.js   >> $popup
 cat src/popup.js              >> $popup
 
+\cp -f $content dist/firefox/js/content.js
+\cp -f $popup   dist/firefox/js/popup.js
+
+\cp -f manifest.json dist/chrome/
+jq -s '.[0] * .[1]' manifest.json manifest_firefox.json > dist/firefox/manifest.json
+
+for brw in `ls dist`
+do
+  \cp src/csrf-setter.js dist/$brw/js/
+  \cp src/sanitizer.js dist/$brw/js/
+  \cp -r *.html dist/$brw/
+  \cp -r icons  dist/$brw/
+done
 echo "build done"
