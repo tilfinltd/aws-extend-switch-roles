183907df26b0d1a765e7bbce780d05b0572412e5
Toshimitsu Takahashi <toshi@tilfin.com>
2018-02-27 02:54:35 +0900

    Add tests

--- test/content_switched.test.js
+++ test/content_switched.test.js
@@ -1,78 +1,48 @@
 describe('ContentScripts', () => {
-  context('when initial user have signed in', () => {
-    beforeEach(() => {
-      loadFixtures('awsmc-iam-initial', 'data');
-    })
-
-    it('does not run generateEmptyRoleList and awsc-username-menu-recent-roles does not exist', () => {
-      expect(document.body.className.includes('user-type-iam')).to.be.true;
-      expect(document.getElementById('awsc-username-menu-recent-roles')).to.be.null;
-    })
-
-    it('creates awsc-username-menu-recent-roles', () => {
-      extendIAMFormList();
-      expect(document.body.className.includes('user-type-iam')).to.be.true;
-      expect(document.getElementById('awsc-username-menu-recent-roles')).to.exist;
-    })
-  })
-
   context('when iam user have signed in with 5 histories', () => {
     it('load base aws account is number', () => {
-      loadFixtures('awsmc-iam');
+      loadFixtures('awsmc-iam-switched');
       extendIAMFormList();
 
-      expect(document.body.className.includes('user-type-iam')).to.be.true;
-      expect(document.body.className.includes('user-type-federated')).to.be.false;
+      expect(document.body.className.includes('user-type-federated')).to.be.true;
+      expect(document.body.className.includes('user-type-iam')).to.be.false;
       expect(document.querySelectorAll('#awsc-username-menu-recent-roles li').length).to.eq(5);
     })
 
     context('not hidesHistory', () => {
-      context('base profile is undefined', () => {
-        it('appends 2 roles but one of them already exists', () => {
-          loadFixtures('awsmc-iam', 'data');
-          chrome.storage.sync.data.hidesHistory = false;
+      context('base-a profile', () => {
+        it('appends 4 roles but two of them already exist', () => {
+          loadFixtures('awsmc-iam-switched', 'data');
+          document.getElementById('awsc-login-display-name-account').textContent = '5555-1111-2222';
           extendIAMFormList();
 
-          expect(document.body.className.includes('user-type-iam')).to.be.true;
-          expect(document.body.className.includes('user-type-federated')).to.be.false;
+          expect(document.body.className.includes('user-type-federated')).to.be.true;
+          expect(document.body.className.includes('user-type-iam')).to.be.false;
 
           const roles = Array.from(document.querySelectorAll('#awsc-username-menu-recent-roles li'))
-          expect(roles.length).to.eq(6);
+          expect(roles.length).to.eq(7);
+          expect(roles[0].querySelector('input[name="roleName"]').value).to.eq('stg-role');
+          expect(roles[0].querySelector('input[type="submit"]').value).to.eq('a-stg  |  555511113333');
           expect(roles[3].querySelector('input[name="roleName"]').value).to.eq('contained_history_role');
           expect(roles[5].querySelector('input[name="roleName"]').value).to.eq('independence_role');
+          expect(roles[6].querySelector('input[name="roleName"]').value).to.eq('prod-role');
+          expect(roles[6].querySelector('input[type="submit"]').value).to.eq('a-prod  |  555511114444');
         })
       })
     })
 
     context('hidesHistory', () => {
-      context('base profile is undefined', () => {
-        it('hides histories and appends 2 roles', () => {
-          loadFixtures('awsmc-iam', 'data');
-          chrome.storage.sync.data.hidesHistory = true;
-          extendIAMFormList();
-
-          expect(document.body.className.includes('user-type-iam')).to.be.true;
-          expect(document.body.className.includes('user-type-federated')).to.be.false;
-
-          const roles = Array.from(document.querySelectorAll('#awsc-username-menu-recent-roles li'))
-          expect(roles.length).to.eq(2);
-          expect(roles[0].querySelector('input[name="roleName"]').value).to.eq('independence_role');
-          expect(roles[1].querySelector('input[name="roleName"]').value).to.eq('contained_history_role');
-        })
-      })
-
       context('base-a profile', () => {
         it('hides histories and appends 4 roles', () => {
-          loadFixtures('awsmc-iam', 'data');
+          loadFixtures('awsmc-iam-switched', 'data');
           document.getElementById('awsc-login-display-name-account').textContent = '5555-1111-2222';
           chrome.storage.sync.data.hidesHistory = true;
           extendIAMFormList();
 
-          expect(document.body.className.includes('user-type-iam')).to.be.true;
-          expect(document.body.className.includes('user-type-federated')).to.be.false;
+          expect(document.body.className.includes('user-type-federated')).to.be.true;
+          expect(document.body.className.includes('user-type-iam')).to.be.false;
 
           const roles = Array.from(document.querySelectorAll('#awsc-username-menu-recent-roles li'))
-          expect(roles.length).to.eq(4);
           expect(roles[0].querySelector('input[name="roleName"]').value).to.eq('independence_role');
           expect(roles[1].querySelector('input[name="roleName"]').value).to.eq('contained_history_role');
           expect(roles[2].querySelector('input[name="roleName"]').value).to.eq('stg-role');
@@ -84,13 +54,13 @@ describe('ContentScripts', () => {
 
       context('base-b profile', () => {
         it('hides histories and appends 5 roles', () => {
-          loadFixtures('awsmc-iam', 'data');
+          loadFixtures('awsmc-iam-switched', 'data');
           document.getElementById('awsc-login-display-name-account').textContent = '6666-1111-2222';
           chrome.storage.sync.data.hidesHistory = true;
           extendIAMFormList();
 
-          expect(document.body.className.includes('user-type-iam')).to.be.true;
-          expect(document.body.className.includes('user-type-federated')).to.be.false;
+          expect(document.body.className.includes('user-type-federated')).to.be.true;
+          expect(document.body.className.includes('user-type-iam')).to.be.false;
 
           const roles = Array.from(document.querySelectorAll('#awsc-username-menu-recent-roles li'))
           expect(roles.length).to.eq(5);
@@ -109,14 +79,14 @@ describe('ContentScripts', () => {
     context('hidesHistory and hidesAccountId', () => {
       context('base-b profile', () => {
         it('hides account id', () => {
-          loadFixtures('awsmc-iam', 'data');
+          loadFixtures('awsmc-iam-switched', 'data');
           document.getElementById('awsc-login-display-name-account').textContent = '6666-1111-2222';
           chrome.storage.sync.data.hidesHistory = true;
           chrome.storage.sync.data.hidesAccountId = true;
           extendIAMFormList();
 
-          expect(document.body.className.includes('user-type-iam')).to.be.true;
-          expect(document.body.className.includes('user-type-federated')).to.be.false;
+          expect(document.body.className.includes('user-type-federated')).to.be.true;
+          expect(document.body.className.includes('user-type-iam')).to.be.false;
 
           const roles = Array.from(document.querySelectorAll('#awsc-username-menu-recent-roles li'))
           expect(roles.length).to.eq(5);
@@ -135,56 +105,48 @@ describe('ContentScripts', () => {
     })
 
     context('showOnlyMatchingRoles', () => {
-      it('does not affect', () => {
-        loadFixtures('awsmc-iam', 'data');
+      it('filters b-roles by the same role name', () => {
+        loadFixtures('awsmc-iam-switched', 'data');
         document.getElementById('awsc-login-display-name-account').textContent = '6666-1111-2222';
         chrome.storage.sync.data.showOnlyMatchingRoles = true;
         extendIAMFormList();
 
-        expect(document.body.className.includes('user-type-iam')).to.be.true;
-        expect(document.body.className.includes('user-type-federated')).to.be.false;
+        expect(document.body.className.includes('user-type-federated')).to.be.true;
+        expect(document.body.className.includes('user-type-iam')).to.be.false;
 
         const roles = Array.from(document.querySelectorAll('#awsc-username-menu-recent-roles li'))
-        expect(roles.length).to.eq(9);
-        expect(roles[3].querySelector('input[name="roleName"]').value).to.eq('contained_history_role');
-        expect(roles[5].querySelector('input[name="roleName"]').value).to.eq('independence_role');
-        expect(roles[6].querySelector('input[name="roleName"]').value).to.eq('stg-role');
-        expect(roles[6].querySelector('input[type="submit"]').value).to.eq('b-stg  |  666611113333');
-        expect(roles[7].querySelector('input[name="roleName"]').value).to.eq('prod-role');
-        expect(roles[7].querySelector('input[type="submit"]').value).to.eq('b-prod  |  666611114444');
-        expect(roles[8].querySelector('input[name="roleName"]').value).to.eq('renpou');
-        expect(roles[8].querySelector('input[type="submit"]').value).to.eq('b-renpou  |  666611115555');
+        expect(roles.length).to.eq(6);
+        expect(roles[2].querySelector('input[name="roleName"]').value).to.eq('contained_history_role');
+        expect(roles[4].querySelector('input[name="roleName"]').value).to.eq('independence_role');
+        expect(roles[5].querySelector('input[name="roleName"]').value).to.eq('stg-role');
+        expect(roles[5].querySelector('input[type="submit"]').value).to.eq('b-stg  |  666611113333');
       })
     })
 
     context('hidesHistory and showOnlyMatchingRoles', () => {
-      it('does not affect', () => {
-        loadFixtures('awsmc-iam', 'data');
+      it('hides histories and filters b-roles by the same role name', () => {
+        loadFixtures('awsmc-iam-switched', 'data');
         document.getElementById('awsc-login-display-name-account').textContent = '6666-1111-2222';
         chrome.storage.sync.data.hidesHistory = true;
         chrome.storage.sync.data.showOnlyMatchingRoles = true;
         extendIAMFormList();
 
-        expect(document.body.className.includes('user-type-iam')).to.be.true;
-        expect(document.body.className.includes('user-type-federated')).to.be.false;
+        expect(document.body.className.includes('user-type-federated')).to.be.true;
+        expect(document.body.className.includes('user-type-iam')).to.be.false;
 
         const roles = Array.from(document.querySelectorAll('#awsc-username-menu-recent-roles li'))
-        expect(roles.length).to.eq(5);
+        expect(roles.length).to.eq(3);
         expect(roles[0].querySelector('input[name="roleName"]').value).to.eq('independence_role');
         expect(roles[1].querySelector('input[name="roleName"]').value).to.eq('contained_history_role');
         expect(roles[2].querySelector('input[name="roleName"]').value).to.eq('stg-role');
         expect(roles[2].querySelector('input[type="submit"]').value).to.eq('b-stg  |  666611113333');
-        expect(roles[3].querySelector('input[name="roleName"]').value).to.eq('prod-role');
-        expect(roles[3].querySelector('input[type="submit"]').value).to.eq('b-prod  |  666611114444');
-        expect(roles[4].querySelector('input[name="roleName"]').value).to.eq('renpou');
-        expect(roles[4].querySelector('input[type="submit"]').value).to.eq('b-renpou  |  666611115555');
       })
     })
   })
 
   context('when federated user have signed in with 5 histories', () => {
     it('load base aws account is number', () => {
-      loadFixtures('awsmc-federated');
+      loadFixtures('awsmc-federated-switched');
       extendIAMFormList();
 
       expect(document.body.className.includes('user-type-iam')).to.be.false;
@@ -193,63 +155,34 @@ describe('ContentScripts', () => {
     })
 
     context('not hidesHistory', () => {
-      context('base profile is undefined', () => {
-        it('appends 2 roles but one of them already exists', () => {
-          loadFixtures('awsmc-federated', 'data');
+      context('base-b profile', () => {
+        it('hides histories and appends 5 roles', () => {
+          loadFixtures('awsmc-federated-switched', 'data');
+          document.getElementById('awsc-login-display-name-account').textContent = '6666-1111-2222';
+          chrome.storage.sync.data.hidesHistory = true;
           extendIAMFormList();
 
-          expect(document.body.className.includes('user-type-iam')).to.be.false;
           expect(document.body.className.includes('user-type-federated')).to.be.true;
+          expect(document.body.className.includes('user-type-iam')).to.be.false;
 
           const roles = Array.from(document.querySelectorAll('#awsc-username-menu-recent-roles li'))
-          expect(roles.length).to.eq(6);
-          expect(roles[3].querySelector('input[name="roleName"]').value).to.eq('contained_history_role');
-          expect(roles[5].querySelector('input[name="roleName"]').value).to.eq('independence_role');
+          expect(roles.length).to.eq(5);
+          expect(roles[0].querySelector('input[name="roleName"]').value).to.eq('independence_role');
+          expect(roles[1].querySelector('input[name="roleName"]').value).to.eq('contained_history_role');
+          expect(roles[2].querySelector('input[name="roleName"]').value).to.eq('stg-role');
+          expect(roles[2].querySelector('input[type="submit"]').value).to.eq('b-stg  |  666611113333');
+          expect(roles[3].querySelector('input[name="roleName"]').value).to.eq('prod-role');
+          expect(roles[3].querySelector('input[type="submit"]').value).to.eq('b-prod  |  666611114444');
+          expect(roles[4].querySelector('input[name="roleName"]').value).to.eq('renpou');
+          expect(roles[4].querySelector('input[type="submit"]').value).to.eq('b-renpou  |  666611115555');
         })
       })
     })
 
     context('hidesHistory', () => {
-      context('base profile is undefined', () => {
-        it('hides histories and appends 2 roles', () => {
-          loadFixtures('awsmc-federated', 'data');
-          chrome.storage.sync.data.hidesHistory = true;
-          extendIAMFormList();
-
-          expect(document.body.className.includes('user-type-iam')).to.be.false;
-          expect(document.body.className.includes('user-type-federated')).to.be.true;
-
-          const roles = Array.from(document.querySelectorAll('#awsc-username-menu-recent-roles li'))
-          expect(roles.length).to.eq(2);
-          expect(roles[0].querySelector('input[name="roleName"]').value).to.eq('independence_role');
-          expect(roles[1].querySelector('input[name="roleName"]').value).to.eq('contained_history_role');
-        })
-      })
-
-      context('base-a profile', () => {
-        it('hides histories and appends 4 roles', () => {
-          loadFixtures('awsmc-federated', 'data');
-          document.getElementById('awsc-login-display-name-account').textContent = '5555-1111-2222';
-          chrome.storage.sync.data.hidesHistory = true;
-          extendIAMFormList();
-
-          expect(document.body.className.includes('user-type-federated')).to.be.true;
-          expect(document.body.className.includes('user-type-iam')).to.be.false;
-
-          const roles = Array.from(document.querySelectorAll('#awsc-username-menu-recent-roles li'))
-          expect(roles.length).to.eq(4);
-          expect(roles[0].querySelector('input[name="roleName"]').value).to.eq('independence_role');
-          expect(roles[1].querySelector('input[name="roleName"]').value).to.eq('contained_history_role');
-          expect(roles[2].querySelector('input[name="roleName"]').value).to.eq('stg-role');
-          expect(roles[2].querySelector('input[type="submit"]').value).to.eq('a-stg  |  555511113333');
-          expect(roles[3].querySelector('input[name="roleName"]').value).to.eq('prod-role');
-          expect(roles[3].querySelector('input[type="submit"]').value).to.eq('a-prod  |  555511114444');
-        })
-      })
-
       context('base-b profile', () => {
         it('hides histories and appends 5 roles', () => {
-          loadFixtures('awsmc-federated', 'data');
+          loadFixtures('awsmc-federated-switched', 'data');
           document.getElementById('awsc-login-display-name-account').textContent = '6666-1111-2222';
           chrome.storage.sync.data.hidesHistory = true;
           extendIAMFormList();
@@ -273,7 +206,7 @@ describe('ContentScripts', () => {
 
     context('showOnlyMatchingRoles', () => {
       it('appends 3 roles but one of them already exists', () => {
-        loadFixtures('awsmc-federated', 'data');
+        loadFixtures('awsmc-federated-switched', 'data');
         document.getElementById('awsc-login-display-name-account').textContent = '6666-1111-2222';
         chrome.storage.sync.data.showOnlyMatchingRoles = true;
         extendIAMFormList();
@@ -282,17 +215,17 @@ describe('ContentScripts', () => {
         expect(document.body.className.includes('user-type-iam')).to.be.false;
 
         const roles = Array.from(document.querySelectorAll('#awsc-username-menu-recent-roles li'))
-        expect(roles.length).to.eq(7);
-        expect(roles[3].querySelector('input[name="roleName"]').value).to.eq('contained_history_role');
+        expect(roles.length).to.eq(6);
+        expect(roles[0].querySelector('input[name="roleName"]').value).to.eq('renpou');
+        expect(roles[0].querySelector('input[type="submit"]').value).to.eq('b-renpou  |  666611115555');
+        expect(roles[4].querySelector('input[name="roleName"]').value).to.eq('contained_history_role');
         expect(roles[5].querySelector('input[name="roleName"]').value).to.eq('independence_role');
-        expect(roles[6].querySelector('input[name="roleName"]').value).to.eq('renpou');
-        expect(roles[6].querySelector('input[type="submit"]').value).to.eq('b-renpou  |  666611115555');
       })
     })
 
     context('hidesHistory and showOnlyMatchingRoles', () => {
-      it('appends 3 roles', () => {
-        loadFixtures('awsmc-federated', 'data');
+      it('appends 3 roles but one of them already exists', () => {
+        loadFixtures('awsmc-federated-switched', 'data');
         document.getElementById('awsc-login-display-name-account').textContent = '6666-1111-2222';
         chrome.storage.sync.data.hidesHistory = true;
         chrome.storage.sync.data.showOnlyMatchingRoles = true;
