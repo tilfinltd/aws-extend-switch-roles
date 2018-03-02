061a260da41abeb58a69583395eba63bd99e7f42
Toshimitsu Takahashi <toshi@tilfin.com>
2018-03-03 02:36:42 +0900

    Add tests of auto assume last role

---
+++ test/content_auto_assume_last_role.test.js
@@ -0,0 +1,46 @@
+describe('Auto assume last assumed role on content script', () => {
+  it('triggers clicking the submit button of target role form', (done) => {
+    loadFixtures('awsmc-iam', 'data');
+    document.getElementById('awsc-login-display-name-account').textContent = '5555-1111-2222';
+    chrome.storage.sync.data.autoAssumeLastRole = true;
+    chrome.storage.sync.data.lastRole_555511112222_tilfin = '555511113333_stg-role';
+    extendIAMFormList();
+    const form = document.querySelector('#awsc-username-menu-recent-roles li:nth-child(7)')
+    form.onsubmit = () => {
+      done();
+      return false;
+    }
+  })
+
+  context('hides histories', () => {
+    it('triggers clicking the submit button of target role form', (done) => {
+      loadFixtures('awsmc-iam', 'data');
+      document.getElementById('awsc-login-display-name-account').textContent = '5555-1111-2222';
+      chrome.storage.sync.data.hidesHistory = true;
+      chrome.storage.sync.data.autoAssumeLastRole = true;
+      chrome.storage.sync.data.lastRole_555511112222_tilfin = '555511113333_stg-role';
+      extendIAMFormList();
+      const form = document.querySelector('#awsc-username-menu-recent-roles li:nth-child(3)')
+      form.onsubmit = () => {
+        done();
+        return false;
+      }
+    })
+  })
+
+  context('not autoAssumeLastRole', () => {
+    it('does not trigger clicking any submit button', (done) => {
+      loadFixtures('awsmc-iam', 'data');
+      document.getElementById('awsc-login-display-name-account').textContent = '5555-1111-2222';
+      chrome.storage.sync.data.autoAssumeLastRole = false;
+      chrome.storage.sync.data.lastRole_555511112222_tilfin = '555511113333_stg-role';
+      extendIAMFormList();
+      const form = document.querySelector('#awsc-username-menu-recent-roles li:nth-child(7)')
+      form.onsubmit = () => {
+        done('must not occurr');
+        return false;
+      }
+      done();
+    })
+  })
+})
