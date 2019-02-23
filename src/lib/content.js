a1e1c435159593552d89db03c853d1486523fdd6
Toshimitsu Takahashi <toshi@tilfin.com>
2019-02-24 00:13:30 +0900

    Fix insert html and version (#79)
    * Fix inserting html
    
    * Bump 0.11.0

--- src/lib/content.js
+++ src/lib/content.js
@@ -220,7 +220,7 @@ function attachColorLine(profiles) {
 
     var label = usernameMenu.querySelector('.nav-elt-label');
     if (found && found.image) {
-        label.insertAdjacentHTML('beforebegin', Sanitizer.escapeHTML(`<img id="AESW_Image" src=${found.image.replace(/"/g, '')} style="float: left; padding-right: 1em; width: 1em; height: 1em">`));
+        label.insertAdjacentHTML('beforebegin', Sanitizer.escapeHTML`<img id="AESW_Image" src=${found.image.replace(/"/g, '')} style="float: left; padding-right: 1em; width: 1em; height: 1em">`);
     }
 
     if (color) {
