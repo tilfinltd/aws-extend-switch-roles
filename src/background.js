f923ca9850d820a83acfbb6e1893fdda569c3117
Toshimitsu Takahashi <toshi@tilfin.com>
2019-07-18 01:26:41 +0900

    Implement extension api for config sender

---
+++ src/background.js
@@ -0,0 +1,60 @@
+function saveAwsConfig(data, callback) {
+  const rawstr = data;
+
+  try {
+    const profiles = loadAwsConfig(rawstr);
+    if (profiles.length > 200) {
+      callback({
+        result: 'failure',
+        error: {
+          message: 'The number of profiles exceeded maximum 200.'
+        }
+      });
+      return;
+    }
+
+    const dps = new DataProfilesSplitter();
+    const dataSet = dps.profilesToDataSet(profiles);
+    dataSet.lztext = LZString.compressToUTF16(rawstr);
+
+    chrome.storage.sync.set(dataSet,
+      function() {
+        callback({ result: 'success' });
+      });
+  } catch (e) {
+    callback({
+      result: 'failure',
+      error: {
+        message: e.message
+      }
+    });
+  }
+}
+
+chrome.runtime.onMessageExternal.addListener(function (message, sender, sendResponse) {
+  const { action, dataType, data } = message || {};
+  if (!action || !dataType || !data) return;
+
+  if (action !== 'updateConfig') {
+    sendResponse({
+      result: 'failure',
+      error: { message: 'Invalid action' }
+    });
+    return;
+  }
+
+  if (dataType !== 'ini') {
+    sendResponse({
+      result: 'failure',
+      error: { message: 'Invalid dataType' }
+    });
+    return;
+  }
+
+  chrome.storage.sync.get(['configSenderId'], function(settings) {
+    const configSenderIds = (settings.configSenderId || '').split(',');
+    if (configSenderIds.includes(sender.id)) {
+      saveAwsConfig(data, sendResponse)
+    }
+  })
+})
