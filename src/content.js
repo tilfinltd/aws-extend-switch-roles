6139ca0d0f576995782c7a222d1db9d69af5a51d
Toshimitsu Takahashi <toshi@tilfin.com>
2020-09-26 20:45:27 +0900

    fix to extract switchrole URL

--- src/content.js
+++ src/content.js
@@ -26,23 +26,34 @@ function adjustDisplayNameColor() {
   }
 }
 
+function extractBackURL() {
+  let swLink = document.querySelector('#menu--account *[data-testid="awsc-switch-roles"]');
+  if (swLink) {
+    return new URL(swLink.href)
+  }
+  swLink = document.getElementById('awsc-exit-role-form');
+  if (swLink) {
+    return new URL(swLink.getAttribute('action'))
+  }
+  return null;
+}
+
 function appendAESRForm(tryCount) {
-  let switchLink= document.querySelector('#menu--account *[data-testid="awsc-switch-roles"]');
-  if (!switchLink) {
+  let backUrl = extractBackURL();
+  if (!backUrl) {
     if (tryCount === 2) return;
     setTimeout(() => {
       appendAESRForm(++tryCount);
     }, 100);
   }
 
-  const actionHost = new URL(switchLink.href).host;
+  const actionHost = new URL(backUrl.href).host;
   const form = document.createElement('form');
   form.id = 'AESR_form';
   form.method = 'POST';
   form.action = `https://${actionHost}/switchrole`;
   form.innerHTML = '<input type="hidden" name="mfaNeeded" value="0"><input type="hidden" name="action" value="switchFromBasis"><input type="hidden" name="src" value="nav"><input type="hidden" name="csrf"><input type="hidden" name="roleName"><input type="hidden" name="account"><input type="hidden" name="color"><input type="hidden" name="redirect_uri"><input type="hidden" name="displayName">';
   document.body.appendChild(form)
-
 }
 
 if (document.body) {
