a231a6f51df26c0bc7e9a784e7f3d51a959951a5
Toshimitsu Takahashi <toshi@tilfin.com>
2021-02-11 13:20:46 +0900

    parse ini format and aws config definitions in detail

--- src/lib/load_aws_config.js
+++ src/lib/load_aws_config.js
@@ -1,56 +1,74 @@
-function parseKeyValue(line) {
-  var strs = line.split('=', 2);
-  var key = strs[0].trim();
-  var val = strs[1].trim();
-  return { key: key, value: val };
-}
+class AwsConfigIniParser {
+  load(text) {
+    const lines = text.split(/\r\n|\r|\n/);
 
-function brushProfile(item) {
-  if (item.role_arn) {
-    var parts = item.role_arn.split('/');
-    var iams = parts.shift().split(':');
+    this.profiles = [];
+    this.item = null;
+    lines.forEach(line => { this.parseLine(line) });
+    this.appendProfile();
 
-    item.aws_account_id = iams[4];
-    item.role_name = parts.join('/');
-
-    delete item.role_arn;
+    return this.profiles;
   }
 
-  return item;
+  parseLine(line) {
+    line = line.replace(/[\;\#].*$/, '').trim() // trim comment and spaces
+    if (line.length === 0) return; // skip empty line
+
+    const item = this.item;
+    const md = line.match(/^\[(.+)\]$/);
+    if (md) {
+      this.appendProfile();
+      this.newProfile(md[1].trim());
+    } else if (item) {
+      const field = this.parseKeyValue(line);
+      if (field.key in item) throw new Error(`duplicate definition of ${field.key}`)
+      item[field.key] = field.value;
+    } else {
+      throw new Error('profile is not declared before the key property definitions')
+    }
+  }
+
+  trimComment(str) {
+    return str.replace(/[;#].*$/, '')
+  }
+
+  parseKeyValue(line) {
+    const [key, val] = line.split('=', 2);
+    if (val === undefined) throw new Error('invalid key property definition')
+    return { key: key.trim(), value: val.trim() };
+  }
+
+  newProfile(name) {
+    const pname = name.replace(/^profile\s+/i, '');
+    this.item = { profile: pname };
+  }
+
+  appendProfile() {
+    if (!this.item) return;
+
+    const profile = this.brushAwsProfile(this.item);
+    this.profiles.push(profile);
+    this.item = null;
+  }
+
+  brushAwsProfile(item) {
+    if (item.role_arn) {
+      const parts = item.role_arn.split('/');
+      const iams = parts.shift().split(':');
+
+      item.aws_account_id = iams[4];
+      item.role_name = parts.join('/');
+
+      delete item.role_arn;
+    }
+
+    if (!item.aws_account_id) throw new Error('invalid profile definition whose the AWS account id is not specified')
+
+    return item;
+  }
 }
 
-
 export function loadAwsConfig(text) {
-  var profiles = [];
-  var lines = text.split(/\r\n|\r|\n/);
-
-  var item = null;
-  lines.forEach(function(rawline) {
-    var line = rawline.trim();
-    var field, r;
-
-    var firstChar = line.charAt(0);
-    if (firstChar === ';' || firstChar === '#') {
-      line = ''; // Comment Line
-    }
-
-    if (r = line.match(/^\[(.+)\]$/)) {
-      if (item) profiles.push(brushProfile(item));
-
-      var pname = r[1].trim();
-      pname = pname.replace(/^profile\s+/i, '');
-      item = { profile: pname };
-    } else if (item) {
-      if (line.length > 0) {
-        var field = parseKeyValue(line);
-        item[field.key] = field.value;
-      } else {
-        profiles.push(brushProfile(item));
-        item = null;
-      }
-    }
-  })
-  if (item) profiles.push(brushProfile(item));
-
-  return profiles;
+  const parser = new AwsConfigIniParser()
+  return parser.load(text);
 }
