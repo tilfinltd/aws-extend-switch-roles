cd9de1608730d52e27367f87c8254c9397210fef
Toshimitsu Takahashi <toshi@tilfin.com>
2022-01-09 22:50:03 +0900

    remove isGlobal

--- src/attach_target.js
+++ src/attach_target.js
@@ -6,14 +6,12 @@
   const accInfo = ConsoleNavService.AccountInfo;
   if (accInfo) {
     Object.assign(info, accInfo);
-    info.isGlobal = document.querySelector('#menu--regions li a') == null;
   } else {
     Object.assign(info, {
       loginDisplayNameAccount: trimTxtCtn(elById('awsc-login-display-name-account')),
       loginDisplayNameUser: trimTxtCtn(elById('awsc-login-display-name-user')),
       roleDisplayNameAccount: trimTxtCtn(elById('awsc-role-display-name-account')),
       roleDisplayNameUser: trimTxtCtn(elById('awsc-role-display-name-user')),
-      isGlobal: document.querySelector('#regionMenuContent a') == null,
     })
   }
 
