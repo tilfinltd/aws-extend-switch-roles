510c05d1facd17587086412df240fe912aaa1b5f
Toshimitsu Takahashi <toshi@tilfin.com>
2023-10-15 21:27:29 +0900

    Update archive script

--- bin/archive.sh
+++ bin/archive.sh
@@ -2,19 +2,29 @@
 #--
 # archive.sh
 #--
-zipfile=aws-extend-switch-roles.zip
+copydest=$1
 
 cd dist/chrome;
+version=`cat manifest.json | jq -r '.version' | sed 's/\./-/g'`
+zipfile="aesr-chrome-$version.zip"
 \rm $zipfile
 zip -r $zipfile \
   manifest.json *.html icons/ js/ 
 echo "archived: chrome/$zipfile"
+if [ "$copydest" != "" ]; then
+  \cp $zipfile $copydest
+fi
 
 echo "----"
 
 cd ../firefox;
+version=`cat manifest.json | jq -r '.version' | sed 's/\./-/g'`
+zipfile="aesr-firefox-$version.zip"
 \rm $zipfile
 zip -r $zipfile \
   manifest.json *.html icons/ js/ 
 
 echo "archived: firefox/$zipfile"
+if [ "$copydest" != "" ]; then
+  \cp $zipfile $copydest
+fi
